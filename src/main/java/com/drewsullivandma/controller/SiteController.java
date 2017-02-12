419627e9ed391a92dc2536b7f7aa17cf56f1fe3a
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-02-12 14:50:33 -0500

    Pushing to prod

--- src/main/java/com/drewsullivandma/controller/SiteController.java
+++ src/main/java/com/drewsullivandma/controller/SiteController.java
@@ -2,21 +2,16 @@ package com.drewsullivandma.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.drewsullivandma.model.Book;
 import com.drewsullivandma.model.BookDAO;
 import com.drewsullivandma.model.Category;
 import com.drewsullivandma.model.CategoryDAO;
 //import com.drewsullivandma.model.InputParser;
-import com.drewsullivandma.model.InputParser;
 
 @Controller 
 public class SiteController {
@@ -49,13 +44,13 @@ public class SiteController {
 	}
 
 //	WIP:
-	@RequestMapping(path="/bookRecommendations", method=RequestMethod.POST)
-	public String processBookSubmission(@RequestParam Map<String, String> formInput) {
-		InputParser ip = new InputParser();
-		Book bookRecordToSave = ip.getBookRecord(formInput);
-		bookDAO.saveNewBook(bookRecordToSave);
-		return "bookRecommendations";
-	}
+//	@RequestMapping(path="/bookRecommendations", method=RequestMethod.POST)
+//	public String processBookSubmission(@RequestParam Map<String, String> formInput) {
+//		InputParser ip = new InputParser();
+//		Book bookRecordToSave = ip.getBookRecord(formInput);
+//		bookDAO.saveNewBook(bookRecordToSave);
+//		return "bookRecommendations";
+//	}
 	
 	@RequestMapping("/pastLife")
 	public String displayPastLife() {
