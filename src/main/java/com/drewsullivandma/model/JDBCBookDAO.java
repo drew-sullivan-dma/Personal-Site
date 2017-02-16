ba1d7a2ce526d89eaad45b12ccedd5a063ae9385
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-02-16 08:26:56 -0500

    Implemented the classes and methods for inserting records into the database

--- src/main/java/com/drewsullivandma/model/JDBCBookDAO.java
+++ src/main/java/com/drewsullivandma/model/JDBCBookDAO.java
@@ -25,11 +25,11 @@ public class JDBCBookDAO implements BookDAO {
 		insertBook(book);
 		int latestBookId = getLatestBookId();
 		List<Category> categories = book.getCategories();
-//		List<Author> authors = book.getAuthors();
-//		List<Description> descriptions = book.getDescriptions();
+		List<Author> authors = book.getAuthors();
+		List<Description> descriptions = book.getDescriptions();
 		insertBookCategoryRelationship(categories, latestBookId);
-//		insertAuthorBookRelationship(authors, latestBookId);
-//		insertBookDescriptionRelationship(descriptions, latestBookId);
+		insertAuthorBookRelationship(authors, latestBookId);
+		insertBookDescriptionRelationship(descriptions, latestBookId);
 	}
 
 	private void insertBook(Book book) {
@@ -38,72 +38,91 @@ public class JDBCBookDAO implements BookDAO {
 		jdbcTemplate.update(sqlSaveNewBook, book.getTitle());
 	}
 	
-	private int getLatestBookId() {
-		String sqlGetLatestBookId = "SELECT max(book_id) "
-						   		  + "FROM book";
-		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetLatestBookId);
-		result.next();
-		int latestId = result.getInt(1);
-		return latestId;
-	}
-	
 	private void insertBookCategoryRelationship(List<Category> categories, int newBookId) {
 		for(Category category : categories) {
 			String sqlSaveNewBookCategoryRelationship = "INSERT INTO book_category(book_id, category_id) "
 							  						  + "VALUES(?,?)";
-			System.out.println("New Book ID: " + newBookId);
-			System.out.println("Category ID: " + category.getCategoryId());
 			jdbcTemplate.update(sqlSaveNewBookCategoryRelationship, newBookId, category.getCategoryId());
+		}
+	}
+	
+	private void insertAuthorBookRelationship(List<Author> authors, int latestBookId) {
+		for(Author author : authors) {
+			String sqlSaveNewAuthor = "INSERT INTO author(first_name, middle_initials, last_name, post_nominal_initials)"
+								    + "VALUES(?,?,?,?)";
+			jdbcTemplate.update(sqlSaveNewAuthor, author.getFirstName(), author.getMiddleInitials(), author.getLastName(), author.getPostNominalInitials());
+
+			int latestAuthorId = getLatestAuthorId();
+			String sqlSaveNewAuthorBookRelationship = "INSERT INTO author_book(author_id, book_id) "
+													+ "VALUES(?,?)";
+			jdbcTemplate.update(sqlSaveNewAuthorBookRelationship, latestBookId, latestAuthorId);
 		}
 	}
 	
 	private void insertBookDescriptionRelationship(List<Description> descriptions, int newBookId) {
 		for(Description description : descriptions) {
-			int newDescriptionId = getNextDescriptionId();
 			String sqlSaveNewDescription = "INSERT INTO description(description) "
 									     + "VALUES(?)";
+			jdbcTemplate.update(sqlSaveNewDescription, description.getDescription());
+			
+			int latestDescriptionId = getLatestDescriptionId();
 			String sqlSaveNewBookDescriptionRelationship = "INSERT INTO book_description(book_id, description_id) "
 														 + "VALUES(?,?)";
-			jdbcTemplate.update(sqlSaveNewDescription, description.getDescription());
-			jdbcTemplate.update(sqlSaveNewBookDescriptionRelationship, newBookId, newDescriptionId);
+			jdbcTemplate.update(sqlSaveNewBookDescriptionRelationship, newBookId, latestDescriptionId);
 		}
 	}
-
-	private void insertAuthorBookRelationship(List<Author> authors, int newBookId) {
-		for(Author author : authors) {
-			int newAuthorId = getNextAuthorId();
-			String sqlSaveNewAuthor = "INSERT INTO author(first_name, middle_initials, last_name, post_nominal_initials)"
-								    + "VALUES(?,?,?,?)";
-			String sqlSaveNewAuthorBookRelationship = "INSERT INTO author_book(author_id, book_id) "
-													+ "VALUES(?,?)";
-			jdbcTemplate.update(sqlSaveNewAuthor, author.getFirstName(), author.getMiddleInitials(), author.getLastName(), author.getPostNominalInitials());
-			jdbcTemplate.update(sqlSaveNewAuthorBookRelationship, newBookId, newAuthorId);
-		}
-	}
-
-	private int getNextBookId() {
-		String sqlGetNextBookId = "SELECT nextval('book_book_id_seq')";
-		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetNextBookId);
+	
+	private int getLatestBookId() {
+		String sqlGetLatestBookId = "SELECT max(book_id) "
+						   		  + "FROM book";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetLatestBookId);
 		result.next();
-		int id = result.getInt(1);
-		System.out.println("Before being called: " + id);
-		return id;
+		int latestBookId = result.getInt(1);
+		return latestBookId;
 	}
 	
-	private int getNextAuthorId() {
-		String sqlGetNextAuthorId = "SELECT nextval('author_author_id_seq')";
-		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetNextAuthorId);
+	private int getLatestAuthorId() {
+		String sqlGetLatestAuthorId = "SELECT max(author_id) "
+						   		    + "FROM author";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetLatestAuthorId);
 		result.next();
-		int id = result.getInt(1);
-		return id;
+		int latestAuthorId = result.getInt(1);
+		return latestAuthorId;
 	}
 	
-	private int getNextDescriptionId() {
-		String sqlGetNextDescriptionId = "SELECT nextval('description_description_id_seq')";
-		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetNextDescriptionId);
+	private int getLatestDescriptionId() {
+		String sqlGetLatestDescriptionId = "SELECT max(description_id) "
+						   		  		 + "FROM description";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetLatestDescriptionId);
 		result.next();
-		int id = result.getInt(1);
-		return id;
+		int latestDescriptionId = result.getInt(1);
+		return latestDescriptionId;
 	}
+	
+
+//	private int getNextBookId() {
+//		String sqlGetNextBookId = "SELECT nextval('book_book_id_seq')";
+//		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetNextBookId);
+//		result.next();
+//		int id = result.getInt(1);
+//		System.out.println("Before being called: " + id);
+//		return id;
+//	}
+//	
+//	private int getNextAuthorId() {
+//		String sqlGetNextAuthorId = "SELECT nextval('author_author_id_seq')";
+//		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetNextAuthorId);
+//		result.next();
+//		int id = result.getInt(1);
+//		return id;
+//	}
+//	
+//	private int getNextDescriptionId() {
+//		String sqlGetNextDescriptionId = "SELECT nextval('description_description_id_seq')";
+//		SqlRowSet result = jdbcTemplate.queryForRowSet(sqlGetNextDescriptionId);
+//		result.next();
+//		int id = result.getInt(1);
+//		return id;
+//	}
 
 }
