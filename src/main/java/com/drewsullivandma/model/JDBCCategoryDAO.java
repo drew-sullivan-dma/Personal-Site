84e54201e318e71d5850d7dca49f88816729b2b9
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-04-09 13:52:49 -0400

    ordered categories alphabetically

--- src/main/java/com/drewsullivandma/model/JDBCCategoryDAO.java
+++ src/main/java/com/drewsullivandma/model/JDBCCategoryDAO.java
@@ -24,7 +24,8 @@ public class JDBCCategoryDAO implements CategoryDAO{
 	public List<Category> getAllCategories() {
 		List<Category> categoryList = new ArrayList<>();
 		SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT * "
-													  + "FROM category;");
+													  + "FROM category "
+													  + "ORDER BY name ASC;");
 		while(results.next()) {
 			Category c = new Category();
 			c = mapRowToCategory(results);
