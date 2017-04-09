fdf1848711482dc66e1b0ac299631a477dc7d0d8
Drew Sullivan <drew.sullivan.dma@gmail.com>
2017-04-09 14:06:37 -0400

    removed excess methods from siteController

--- src/main/java/com/drewsullivandma/controller/SiteController.java
+++ src/main/java/com/drewsullivandma/controller/SiteController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +17,6 @@ import com.drewsullivandma.model.BookDAO;
 import com.drewsullivandma.model.Category;
 import com.drewsullivandma.model.CategoryDAO;
 import com.drewsullivandma.model.InputParser;
-import com.drewsullivandma.model.LoginBean;
 
 @Controller 
 public class SiteController {
@@ -41,27 +39,6 @@ public class SiteController {
 	public String displayAboutMe() {
 		return "aboutMe";
 	}
-	
-//	@RequestMapping(path="/login", method=RequestMethod.GET)
-//    public String init(ModelMap model) {
-//        model.addAttribute("welcomeMessage", "Please Enter Your Username and Password");
-//        return "login";
-//    }
-	
-//	@RequestMapping(path="/login", method=RequestMethod.POST)
-//    public String submit(ModelMap model, @ModelAttribute("loginBean") LoginBean loginBean) {
-//        if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) {
-//            if (loginBean.getUserName().toLowerCase().equals("drew") && loginBean.getPassword().equals("4rLsHMW@UW97JvVT*=ACyVww")) {
-//                return "redirect:/bookInputForm";
-//            } else {
-//                model.addAttribute("error", "Incorrect Username or Password");
-//                return "login";
-//            }
-//        } else {
-//            model.addAttribute("error", "Incorrect Username or Password");
-//            return "login";
-//        }
-//    }
 	
 	@RequestMapping("/bookRecommendations")
 	public String displayBookRecommendations(ModelMap model) {
